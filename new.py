e60a6db0e6aa23f6676e98e20300259fc67eb83a
nemitborn <mahir.muentes@bcestudent.com>
2026-09-09 15:59:41 +0100

    Update new.py
    Test number 2

--- new.py
+++ new.py
@@ -1,2 +1,18 @@
-print("me")
-print("another test")
+Ability_List=[]
+def damage(Health):
+    Health-=10
+    return Health
+CharName=input("Enter your character's name: ")
+Ability_Num=int(input("Enter the number of abilities this character has: "))
+for i in range(0,Ability_Num):
+    Ability=input("Enter an ability: ")
+    Ability_List.append(Ability)
+print(CharName,"Has:",Ability_List)
+Health=int(input("Enter health: "))
+print(CharName, "has", Health,"HP")
+Attack=input("Attack?")
+if Attack=="yes":
+    print(damage(Health))
+
+
+
